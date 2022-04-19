4f02b432f3993b196a534f3782318ae7c9f3315b
ErEn <102028302+BrotherhoodErEn@users.noreply.github.com>
2022-04-19 14:31:42 +0300

    Add files via upload

---
+++ body_index/body.js
@@ -0,0 +1,17 @@
+function bmi(kilo, boy){
+    let bmi = kilo/ (boy**2);
+
+if(bmi <18.5){
+    return "Underweight";
+}
+else if(bmi < 25){
+    return "Normal";
+}
+else if(bmi < 30){
+    return "Overweight";
+}
+else {
+    return "Obese";
+}
+
+}
